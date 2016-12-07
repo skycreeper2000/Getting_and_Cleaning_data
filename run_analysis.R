5ddd9c7787af0b4b312570046f364b96da73a50a
Rishi Jain <skycreeper2000@gmail.com>
2016-12-07 09:07:45 +0530

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,47 @@
+#Import the dplyr, reshape and data.table package
+library(dplyr)
+library(reshape2)
+library(data.table)
+
+# Load the various datasets
+test.subject <- read.table("./test/subject_test.txt")
+test.x <- read.table("./test/X_test.txt")
+test.y <- read.table("./test/y_test.txt")
+
+train.subject <- read.table("./train/subject_train.txt")
+train.x <- read.table("./train/X_train.txt")
+train.y <- read.table("./train/y_train.txt")
+
+features <- read.table("./features.txt")
+activity.labels <- read.table("./activity_labels.txt")
+
+
+# Merge the test and train subject datasets
+subject <- rbind(test.subject, train.subject)
+colnames(subject) <- "subject"
+
+# Merge the test and train labels. Applying the textual labels instead of numeric labels
+label <- rbind(test.y, train.y)
+label <- merge(label, activity.labels, by=1)[,2]
+
+# Merge the test and train main dataset, applying the textual headings from features dataset
+data <- rbind(test.x, train.x)
+colnames(data) <- features[, 2]
+
+# Merge all three datasets
+data <- cbind(subject, label, data)
+
+# Create a smaller dataset containing only the mean and std variables. In text we have "-mean" and "-std"
+search <- grep("-mean|-std", colnames(data))
+data.mean.std <- data[,c(1,2,search)]
+
+# Compute the means, grouped by subject/label. 
+#Can also use "summarise_each(funs(mean))" function along with group_by
+melted = melt(data.mean.std, id.var = c("subject", "label"))
+means = dcast(melted , subject + label ~ variable, mean)
+
+# Save the resulting dataset
+write.table(means, file="./tidy_data.txt")
+
+# Output final dataset
+means
